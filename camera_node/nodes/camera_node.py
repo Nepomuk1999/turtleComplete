310accdce0d9f2965cc8d75570c8fc8976aa133e
Jan0608 <jan.wenger@live.de>
2020-01-24 13:32:42 +0100

    updated camera node

--- camera_node/nodes/camera_node.py
+++ camera_node/nodes/camera_node.py
@@ -42,6 +42,8 @@ class CameraController:
         self._current_orientation = None
         rospy.wait_for_message('/odom', Odometry)
         self._phi = 0.0
+        self._found_x = []
+        self._found_y = []
         #rospy.
 
     def control_loop(self):
@@ -51,7 +53,8 @@ class CameraController:
             rospy.wait_for_message('/odom', Odometry)
             current_x = self._current_x
             current_y = self._current_y
-            opening = self.position_token(current_binary_image, current_x, current_y)
+            current_phi = self._phi
+            opening = self.position_token(current_binary_image, current_x, current_y, current_phi)
             #cv2.imshow('opening', opening)
             # cv2.waitKey(1)
 
@@ -60,6 +63,7 @@ class CameraController:
         self._current_x = self._current_pose.position.x
         self._current_y = self._current_pose.position.y
         self._current_orientation = self._current_pose.orientation
+        self.get_rotation(msg)
 
     def image_callback(self, msg):
         bridge = CvBridge()
@@ -67,9 +71,10 @@ class CameraController:
 
             cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
             img_hsv = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2HSV)
-            mask = cv2.inRange(img_hsv, (60, 30, 30), (100, 255, 180))
+            mask = cv2.inRange(img_hsv, (60, 20, 15), (100, 230, 235))
+            #mask = cv2.inRange(img_hsv, (65, 40, 40), (95, 220, 180))
             #croped = cv2.bitwise_and(cv2_img, cv2_img, mask=mask)
-            kernel = np.ones((5, 5), np.float32) / 25
+            kernel = np.ones((3, 3), np.float32) / 25
             self._binary_image = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
             #cv2.imshow('bw', self._binary_image)
             #cv2.waitKey(1)
@@ -82,13 +87,14 @@ class CameraController:
         (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
         self._phi = yaw
 
-    def position_token(self, median, pos_robot_x, pos_robot_y):
+    def position_token(self, median, pos_robot_x, pos_robot_y, pos_robot_phi):
         found = False
         height, width = np.shape(median)
 
         # find contours in the binary image
         img, contours, hierarchy = cv2.findContours(median, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        #output = median.copy()
+        output = median.copy()
+        '''
         #Version 1
         # print contours
         for c in contours:
@@ -111,19 +117,31 @@ class CameraController:
         #Version 2
         if len(contours) != 0:
             # the contours are drawn here
-            cv2.drawContours(output, contours, -1, 155, 3)
+            #cv2.drawContours(output, contours, -1, 155, 3)
 
             # find the biggest area of the contour
             c = max(contours, key=cv2.contourArea)
-            print 'c', c
-            x, y, w, h = cv2.boundingRect(c)
-            # draw the 'human' contour (in green)
-            cv2.rectangle(output, (x, y), (x + w, y + h), (0, 155, 0), 2)
+            # calculate moments for each contour
+            M = cv2.moments(c)
 
-        # display the image
-        cv2.imshow("Image2", output)
-        '''
-        cv2.waitKey(1)
+            # calculate x,y coordinate of center
+            if M["m00"] != 0:
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                found = True
+            else:
+                cX, cY = 0, 0
+
+            cv2.circle(output, (cX, cY), 5, (100, 100, 100), -1)
+            #found = True
+            #x, y, w, h = cv2.boundingRect(c)
+            # draw the 'human' contour (in green)
+            #cv2.rectangle(output, (x, y), (x + w, y + h), (0, 155, 0), 2)
+
+            # display the image
+            cv2.imshow("Image2", output)
+
+            cv2.waitKey(1)
 
         if found == True:
             middel_height = cY  # round((height_1+height_2)/2)
@@ -135,19 +153,26 @@ class CameraController:
                           [19.2638464106271, 738.626135977933, -22187.7158339254],
                           [0.0143717792767875, 0.514067001440494, 1]])
             point_2 = H.dot(point_1)
-            px = point_2[0] / point_2[2]
-            py = point_2[1] / point_2[2]
-            px_robot = (1200 / 2 - px)/1000
-            py_robot = (255 + 1200 - py)/1000
+            py = point_2[0] / point_2[2]
+            px = point_2[1] / point_2[2]
+            py_robot = (1200 / 2 - py)/1000
+            px_robot = (255 + 1200 - px)/1000
 
             token_rob = np.array([px_robot, py_robot, 1])
             print 'token_rob', token_rob
 
             # ToDO check if x and y are ok
-            T = np.array([[math.cos(self._phi), -math.sin(self._phi), pos_robot_y],
-                          [math.sin(self._phi), math.cos(self._phi), pos_robot_x],
+            T = np.array([[math.cos(pos_robot_phi), -math.sin(pos_robot_phi), pos_robot_x],
+                          [math.sin(pos_robot_phi), math.cos(pos_robot_phi), pos_robot_y],
                           [0, 0, 1]])
             token_glob = T.dot(token_rob)
+            self._found_x = np.append(self._found_x,token_glob[0])
+            self._found_y = np.append(self._found_y,token_glob[1])
+            print 'len', self._found_x
+            if len(self._found_x)%50 == 0:
+                plt.plot(self._found_x, self._found_y, 'ro')
+                plt.show()#block=False)
+            print 'phi: ', pos_robot_phi
             print 'token_glob', token_glob
             print 'px_robot:', pos_robot_x
             print 'py_robot:', pos_robot_y
